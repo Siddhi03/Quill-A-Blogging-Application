feb4a1dd16938250e86728269e8269cf4fdc4e6f
Siddhi Jain <jainsiddhi03@gmail.com>
2024-05-15 00:04:41 +0530

    added separate routes for user and blog with jwt authentication

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from 'hono'
-import {PrismaClient} from "@prisma/client/edge"
-import { withAccelerate } from '@prisma/extension-accelerate'
-import { decode, sign, verify } from 'hono/jwt'
+import { userRouter } from './routes/user';
+import { blogRouter } from './routes/blog';
+
 
 const app = new Hono<{
   Bindings: {
@@ -10,87 +10,10 @@ const app = new Hono<{
   }
 }>()
 
-app.post('/api/v1/user/signup', async(c) =>{
+app.route("/api/v1/user", userRouter)
+app.route("/api/v1/blog", blogRouter)
 
-  const body = await c.req.json();
-  const prisma = new PrismaClient({
-    datasourceUrl:c.env.DATABASE_URL,
-  }).$extends(withAccelerate())
 
-  try{
-    const user = await prisma.user.create({
-      data: {
-        email: body.email,
-        password: body.password,
-        name: body.name
-      }
-    })
-
-    const jwt = await sign({
-      id: user.id
-    }, c.env.JWT_SECRET);
-
-    return c.text(jwt);
-
-  } catch(e){
-    c.status(411);
-    return c.text("Invalid")
-  }
-  
-
-})
-
-app.post('/api/v1/user/signin', async(c) =>{
-  const body = await c.req.json();
-  const prisma = new PrismaClient({
-    datasourceUrl:c.env.DATABASE_URL,
-  }).$extends(withAccelerate())
-
-  try{
-    const user = await prisma.user.findFirst({
-      where: {
-        email: body.email,
-        password: body.password,
-      }
-    })
-
-    if(!user){
-      c.status(403);
-      return c.json({
-        message: "Invalid credentials"
-      })
-    }
-
-    const jwt = await sign({
-      id: user.id
-    }, c.env.JWT_SECRET);
-
-    return c.text(jwt);
-    
-  } catch(e){
-    c.status(411);
-    return c.text("Invalid")
-  }
-  return c.text('signin route created')
-})
-
-app.post('/api/v1/blog', (c) =>{
-  return c.text('write blog')
-})
-
-app.put('/api/v1/blog', (c)=>{
-  return c.text("update blog")
-})
-
-app.get('/api/v1/blog', (c)=>{
-  const id = c.req.param('id');
-  console.log(id);
-  return c.text('get blog route')
-})
-
-app.get('/api/v1/blog/bulk', (c)=>{
-  return c.text("hhhh")
-})
 
 
 export default app
